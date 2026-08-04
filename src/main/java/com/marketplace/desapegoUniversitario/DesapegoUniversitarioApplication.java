23b431b91b8d84822dc72d08daff2d052ce67a10
Iuri <yuriaragao160@gmail.com>
2026-08-04 19:54:46 -0300

    feat: conexão com banco de dados

---
+++ src/main/java/com/marketplace/desapegoUniversitario/DesapegoUniversitarioApplication.java
@@ -0,0 +1,16 @@
+package com.marketplace.desapegoUniversitario;
+
+import me.paulschwarz.springdotenv.spring.DotenvApplicationInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class DesapegoUniversitarioApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DesapegoUniversitarioApplication.class)
+			.initializers(new DotenvApplicationInitializer())
+			.run(args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/desapegoUniversitario/DesapegoUniversitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.desapegoUniversitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesapegoUniversitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
