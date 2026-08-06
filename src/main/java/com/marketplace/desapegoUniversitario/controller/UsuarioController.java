df2155e91f8ef68c0761d8f1d71fc538fcb31a6d
Iuri <yuriaragao160@gmail.com>
2026-08-06 14:14:23 -0300

    feat: cria UsuarioController com endpoints de cadastro e login

--- src/main/java/com/marketplace/desapegoUniversitario/controller/UsuarioController.java
+++ src/main/java/com/marketplace/desapegoUniversitario/controller/UsuarioController.java
@@ -1,4 +1,27 @@
 package com.marketplace.desapegoUniversitario.controller;
 
+import com.marketplace.desapegoUniversitario.dto.CadastroDTO;
+import com.marketplace.desapegoUniversitario.dto.LoginDTO;
+import com.marketplace.desapegoUniversitario.dto.UsuarioResponseDTO;
+import com.marketplace.desapegoUniversitario.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/cadastro")
+    public UsuarioResponseDTO cadastrar(@RequestBody CadastroDTO dto) {
+        return usuarioService.cadastrar(dto);
+    }
+
+    @PostMapping("/login")
+    public UsuarioResponseDTO login(@RequestBody LoginDTO dto) {
+        return usuarioService.login(dto);
+    }
 }
